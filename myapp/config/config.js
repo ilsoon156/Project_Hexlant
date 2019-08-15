ce170840685f2e4bf5d1d660b105de3507d358ac
ilsoon156 <ilsoon156@naver.com>
2019-08-15 14:29:42 +0900

    init

---
+++ myapp/config/config.js
@@ -0,0 +1,11 @@
+const config = {
+    address: '',
+    privateKey: '',
+    httpEndpoint: 'https://ropsten.infura.io/v3/mainnet.infura.io/v3/de6f35437a7b4d22bacb895d93918523',
+    etherscan_api_key: ""
+}
+
+
+exports.getConfig = function () {
+    return config;
+}
